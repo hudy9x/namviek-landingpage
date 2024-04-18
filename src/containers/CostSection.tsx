34194ab0217b12ca9562c33d6e6d08aa9e1f8475
hudy9x <hudy9x@gmail.com>
2024-04-18 23:16:56 +0700

    update code

--- src/containers/CostSection.tsx
+++ src/containers/CostSection.tsx
@@ -1,5 +1,6 @@
 import Heading2 from "@/components/Heading2";
 import Para2 from "@/components/Para2";
+import CostSectionContainer from "./CostSectionContainer";
 
 export default function CostSection() {
   return <section className="cost pt-[200px]">
@@ -7,5 +8,6 @@ export default function CostSection() {
       <Heading2 className="text-center">Decrease the cost<br />to less than $15 per month</Heading2>
       <Para2 className="text-center">Ultilize free platforms and low budget aws services<br />to host the app</Para2>
     </header>
+    <CostSectionContainer />
   </section>
 }
