3b21a5d8bb355a718771d990fa51b149cfd19d8a
hudy9x <hudy9x@gmail.com>
2024-09-15 22:32:26 +0700

    try to use polar for payouts

--- src/containers/Pricing/index.tsx
+++ src/containers/Pricing/index.tsx
@@ -4,7 +4,34 @@ import './style.css'
 import Link from 'next/link'
 import { FaCircleCheck } from 'react-icons/fa6'
 
-export default function Pricing() {
+import { Polar } from "@polar-sh/sdk";
+
+const polar = new Polar({
+  accessToken: process.env["POLAR_ACCESS_TOKEN"] ?? "",
+});
+
+async function getProductInfo() {
+  console.log('called')
+
+  const id = "19a6a164-d5a7-4e99-83eb-aa39e4d41de8"
+  const result = await polar.products.get({
+    id
+  })
+
+  console.log(result.prices)
+
+  return {
+    priceAmount: result.prices[0].priceAmount / 100,
+    priceCurrency: result.prices[0].priceCurrency,
+    link: `https://polar.sh/api/checkout?price=${result.prices[0].id}`
+  };
+}
+
+
+// export const revalidate = 10
+export default async function Pricing() {
+  const product = await getProductInfo()
+  console.log('=====', product)
   const whatIncludes = [
     'Codebase',
     'Usage document',
@@ -24,7 +51,7 @@ export default function Pricing() {
       <div className="mt-14 sm:mt-28 flex flex-col sm:flex-row items-center gap-6">
         <Card type='darkblur' className='p-10 w-[95%] sm:w-[648px]' height="h-[460px] sm:h-[400px]">
           <h2 className='font-semibold text-xl'>Lifetime membership</h2>
-          <p className='text-xs sm:text-[17px] mt-6 second-color'>Enjoy unlimited access to our codebase-based project management tool, tailored to your specific needs.</p>
+          <p className='text-xs sm:text-[17px] leading-5 sm:leading-7 mt-6 second-color'>Enjoy unlimited access to our codebase-based project management tool, tailored to your specific needs.</p>
           <h3 className='flex items-center gap-6 mt-6'>
             <span className='section-title whitespace-nowrap'>{`
               What's included
@@ -45,8 +72,11 @@ export default function Pricing() {
         </Card>
         <Card className='w-[95%] sm:w-[320px] p-10 flex flex-col gap-6 justify-center text-center' height={"h-[400px]"}>
           <p className='uppercase text-sm font-semibold text-[#EDC6CC]'>pay once, own it forever</p>
-          <h2 className='uppercase text-5xl space-x-2 font-extrabold text-[#EEE9FC]'><span>$99</span><small className='text-sm font-normal text-[#DCA9A9]'>USD</small></h2>
-          <Link href={'https://polar.sh/namviek'}>
+          <h2 className='uppercase text-5xl space-x-2 font-extrabold text-[#EEE9FC]'>
+            <span>${product.priceAmount}</span>
+            <small className='text-sm font-normal text-[#DCA9A9]'>{product.priceCurrency}</small>
+          </h2>
+          <Link href={product.link} target='_blank'>
             <button className='cta-btn2 w-full'>Get it now</button>
           </Link>
           <p className='text-[#ECE6FE] text-xs'>
@@ -56,7 +86,7 @@ export default function Pricing() {
         </Card>
 
       </div>
-    </div>
+    </div >
 
     <div className="absolute top-[350px] left-1/2 -translate-x-1/2 -z-10">
 
@@ -64,5 +94,5 @@ export default function Pricing() {
 
 
 
-  </section>
+  </section >
 }
